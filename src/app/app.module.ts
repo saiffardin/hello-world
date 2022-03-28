3e431bc4bbb749d90f53d3e1ae21800b178a99e2
saiffardin <saiffardin@gmail.com>
2022-03-28 14:39:32 +0600

    employee service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { EmpDetailComponent } from './emp-detail/emp-detail.component';
+import { EmpListComponent } from './emp-list/emp-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EmpListComponent, EmpDetailComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
