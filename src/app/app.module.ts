bd006a8872517921316d582b7b6ae4fe9a1feb3d
saiffardin <saiffardin@gmail.com>
2022-03-28 10:50:56 +0600

    two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TestComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,21 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h1>Welcome {{ name }}</h1>
-    <h2 [ngClass]="multiClassObj">AES Angular</h2>
-    <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding 1</h2>
-    <h2 [style.color]="highlightColor">Style Binding 2</h2>
-    <h2 [ngStyle]="titleStyles">Style Binding 3</h2>
-
-    <!-- Event Binding -->
-    <button (click)="onClick($event)">Greet</button>
-    <h3>{{ greetings }}</h3>
-
-    <!-- Template Reference Variables -->
-    <div class="temp-ref-vars">
-        <input #myInput type="text">
-        <button (click)="LogInfo(myInput.value)" >Log</button>
-    </div>
+    <input [(ngModel)] = "name" value="Fardin" type="text" />
+    {{ name }}
   `,
   styles: [
     `
@@ -27,8 +14,8 @@ import { Component, OnInit } from '@angular/core';
         font-size: 2rem;
       }
 
-      .temp-ref-vars{
-          padding: 5px;
+      .temp-ref-vars {
+        padding: 5px;
       }
 
       .text-danger {
@@ -47,36 +34,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
   public name = 'Fardin';
-  public myID = 'textID';
-
-  public hasError = false;
-  public isSpecial = false;
-  public highlightColor = 'orange';
-
-  public greetings = '';
-
-  public multiClassObj = {
-    'text-danger': this.hasError,
-    'text-success': !this.hasError,
-    'text-special': this.isSpecial,
-  };
-
-  public titleStyles = {
-    color: 'blue',
-    fontStyle: 'italic',
-  };
 
   constructor() {}
 
   ngOnInit(): void {}
-
-  onClick(event: any) {
-    console.log('Event Binding');
-    this.greetings = 'Welcome to AES';
-    console.log(event);
-  }
-
-  LogInfo(str:any){
-    console.log(str)
-  }
 }
